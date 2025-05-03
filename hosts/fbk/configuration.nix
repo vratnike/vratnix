b7a7b6ae74a4fc6bdcbb6c031333450bf9d7c1c9
Vratnik <willadd@later>
2025-05-02 20:27:50 -0500

    Open port 1777 on fbk

--- hosts/fbk/configuration.nix
+++ hosts/fbk/configuration.nix
@@ -98,7 +98,7 @@
   services.openssh.enable = true;
 
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 80 443 ];
+  networking.firewall.allowedTCPPorts = [ 80 443 1777];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
