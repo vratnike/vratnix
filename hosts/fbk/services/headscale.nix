ebd316f128a30a29571e03adcf3254abe19661e2
vratnik <vratnik>
2025-09-20 12:10:14 -0500

    Update headscale config with new required option

--- hosts/fbk/services/headscale.nix
+++ hosts/fbk/services/headscale.nix
@@ -5,6 +5,7 @@
     port = 5555;
     serverUrl = "https://headscale.petrichor.moe";
     settings.dns.base_domain = "vratnik.moe";
+    settings.dns.nameservers.global = [ "1.1.1.1" "1.0.0.1" "2606:4700:4700::1111" "2606:4700:4700::1001" ];
     settings = { logtail.enabled = false; };
   };
   services.nginx.virtualHosts = {
