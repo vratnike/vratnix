90c5b8c890ecfb429de47aac63395b2ede7664b0
vratnik <vratnik>
2025-09-20 12:36:12 -0500

    Add experimental features to fbk

--- hosts/fbk/configuration.nix
+++ hosts/fbk/configuration.nix
@@ -10,6 +10,12 @@
     ./services
   ];
 
+  nix = {
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
+
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
